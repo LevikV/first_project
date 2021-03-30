a13065484d7521af07b4f1405a11231589e735a6
LevikV <alex@kplus79.ru>
2021-03-30 22:17:59 +1000

    Исправил синтаксические ошибки в коде программы

--- test.py
+++ test.py
@@ -1,12 +1,12 @@
 import telebot
-bot = telebot.Telebot('1798432712:AAFKD_GMpkCeFhyMFzm6Kg3gSBXABh_jix0')
-@bot.mesage_handler(commands=['start', 'help'])
+bot = telebot.TeleBot('1798432712:AAFKD_GMpkCeFhyMFzm6Kg3gSBXABh_jix0')
+@bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, f'Я бот, приятно познакомиться, {message.from_user.first_name}')
-@bot.mesage_handler(content_types=['text'])
+@bot.message_handler(content_types=['text'])
 def get_text_message(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.from_user.id, 'Привет!')
-    else
+    else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 bot.polling(none_stop=True)
