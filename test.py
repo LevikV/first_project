6ce87234ea45d6d882b74712c82a8c98d02385f0
LevikV <alex@kplus79.ru>
2021-03-31 20:05:15 +1000

    2

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import telebot
 bot = telebot.TeleBot('1798432712:AAFKD_GMpkCeFhyMFzm6Kg3gSBXABh_jix0')
-@bot.message_handler(commands=['start', 'help'])
+
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, f'Я бот, приятно познакомиться, {message.from_user.first_name}')
+    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_message(message.from_user.id, 'Привет, я Яна! Прежде, чем начать наше общение, ты должен пообещать никому об этом не рассказывать)')
 @bot.message_handler(content_types=['text'])
 def get_text_message(message):
     if message.text.lower() == 'привет':
         bot.send_message(message.from_user.id, 'Привет!')
     else:
-        bot.send_message(message.from_user.id, 'Не понимаю, что это значит,хм.....')
+        bot.reply_to(message, 'Не понимаю, что это значит,хм...')
 bot.polling(none_stop=True)
