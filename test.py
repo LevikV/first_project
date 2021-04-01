a061a7de99a39cc8d243761dccc7eed8d61b4535
LevikV <alex@kplus79.ru>
2021-04-01 18:35:30 +1000

    sdf

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ bot = telebot.TeleBot('1798432712:AAFKD_GMpkCeFhyMFzm6Kg3gSBXABh_jix0')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.send_chat_action(message.chat.id, 'typing')
+    bot.send_chat_action(message.chat.id, 'typing') #Показываем индикатор ввода клавиатуры
     keyboard = telebot.types.InlineKeyboardMarkup()
     keyboard.row(
         telebot.types.InlineKeyboardButton('Согласен', callback_data='sog-1'),
@@ -19,9 +19,26 @@ def get_text_message(message):
 #Функция обработчик нажатия кнопок
 @bot.callback_query_handler(func=lambda call: True)
 def iq_callback(query):
+    bot.send_chat_action(query.message.chat.id, 'typing') #Показываем индикатор ввода клавиатуры
     data = query.data
     if data.startswith('sog-1'):
-        bot.send_message(query.message.chat.id, 'Я рада, что ты согласен. Сохранить конфеденциальность для меня очень важно!')
-    bot.edit_message_reply_markup(query.message.chat.id, query.message.message_id)
-
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.row(
+            telebot.types.InlineKeyboardButton('Конечно', callback_data='get-anket'),
+            telebot.types.InlineKeyboardButton('Давай быстрее', callback_data='get-anket')
+        )
+        bot.send_message(query.message.chat.id, 'Я рада, что ты согласился держать в тайне наше общение)) Сохранить конфеденциальность для меня очень важно!')
+        bot.send_message(query.message.chat.id, 'Показать тебе моих хозяек?', reply_markup=keyboard)
+        bot.edit_message_reply_markup(query.message.chat.id, query.message.message_id) #убираем клавиатуру
+    elif data.startswith('sog-0'):
+        keyboard = telebot.types.InlineKeyboardMarkup()
+        keyboard.row(
+            telebot.types.InlineKeyboardButton('Обещаю', callback_data='sog-1'),
+            telebot.types.InlineKeyboardButton('Не обещаю', callback_data='sog-0')
+        )
+        bot.send_message(query.message.chat.id, 'Ммм, так нельзя)) но я как и моя хозяйка, не из стеснительных, спрошу еще раз)) Обещаешь никому не рассказывать про наше общение?', reply_markup=keyboard)
+        bot.edit_message_reply_markup(query.message.chat.id, query.message.message_id)  # убираем клавиатуру
+@bot.message_handler(content_types=['photo'])
+def getidphoto (photo):
+    bot.send_message(photo.message.chat.id, 'Ты загрузил фотка)))')
 bot.polling(none_stop=True)
